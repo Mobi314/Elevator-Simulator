87dbde12d7519b5673c8fbf5645c18bb17c1a5b3
Ckurtz79 <68250381+Ckurtz79@users.noreply.github.com>
2023-04-25 21:15:22 -0400

    Update Elevator.java

--- Elevator.java
+++ Elevator.java
@@ -6,56 +6,101 @@ public class Elevator {
     private DisplayState displayState;
     private int currentFloor;
     private List<User> passengers;
+    private int DELAY_TIME_PER_FLOOR = 200;
+    private int currentWeight;
+    private int MAX_WEIGHT = 3000;// added variable
+
 
     public Elevator(int id) {
-        this.id = id;
-        this.direction = Direction.NONE;
-        this.displayState = DisplayState.IDLE;
-        this.currentFloor = 1;
-        this.passengers = new ArrayList<>();
+      this.id = id;
+      this.direction = Direction.NONE;
+      this.displayState = DisplayState.IDLE;
+      this.currentFloor = 1;
+      this.passengers = new ArrayList<>();
+      this.currentWeight = 0;
     }
+    
+      public boolean hasExceededWeightLimit(User user) {
+        return currentWeight + user.getWeight() > 3000;
+    }
+
+    public boolean hasRoomForUser(User user) {
+      return !hasExceededWeightLimit(user);
+    }
+    public int getCurrentWeight() {
+    int currentWeight = 0;
+    for (User passenger : passengers) {
+        currentWeight += passenger.getWeight();
+    }
+    return currentWeight;
+}
 
     // Getters and setters remain the same
 
+    // If elevator has no passengers, it should remain idle
+    // Move elevator towards the destination floor of the passengers
     public void move(List<User> users) {
-        if (direction == Direction.UP) {
-            currentFloor++;
-        } else if (direction == Direction.DOWN) {
-            currentFloor--;
+    if (direction == Direction.UP) {
+        currentFloor++;
+        if (currentFloor == Building.getFloors()) {
+            direction = Direction.DOWN;
         }
-
-        List<User> arrivedPassengers = new ArrayList<>();
-        for (User passenger : passengers) {
-            if (passenger.getDestinationFloor() == currentFloor) {
-                arrivedPassengers.add(passenger);
-            }
+    } else if (direction == Direction.DOWN) {
+        currentFloor--;
+        if (currentFloor == 1) {
+            direction = Direction.UP;
         }
+    }
 
-        passengers.removeAll(arrivedPassengers);
+    List<User> arrivedPassengers = new ArrayList<>();
+    for (User passenger : passengers) {
+        if (passenger.getDestinationFloor() == currentFloor) {
+            arrivedPassengers.add(passenger);
+        }
+    }
+    passengers.removeAll(arrivedPassengers);
 
-        List<User> pickedUpUsers = new ArrayList<>();
-        for (User user : users) {
-            if (user.getStartFloor() == currentFloor && user.getDirection() == direction) {
+    List<User> pickedUpUsers = new ArrayList<>();
+    for (User user : users) {
+        if (user.getStartFloor() == currentFloor && user.getDirection() == direction) {
+            int newWeight = currentWeight + user.getWeight();
+            if (newWeight <= MAX_WEIGHT) {
                 pickedUpUsers.add(user);
                 passengers.add(user);
+                currentWeight = newWeight;
             }
         }
-
-        users.removeAll(pickedUpUsers);
-
-        if (passengers.isEmpty()) {
-            direction = Direction.NONE;
-        }
     }
+    users.removeAll(pickedUpUsers);
 
-    public boolean hasUsersToPickup(List<User> users) {
-        for (User user : users) {
-            if (user.getDirection() == direction) {
-                return true;
-            }
+    if (passengers.isEmpty()) {
+        if (direction == Direction.NONE && !users.isEmpty()) {
+            direction = users.get(0).getDirection();
         }
-        return false;
+      } else {
+        int maxFloor = passengers.stream().mapToInt(User::getDestinationFloor).max().getAsInt();
+        int minFloor = passengers.stream().mapToInt(User::getDestinationFloor).min().getAsInt();
+        if (currentFloor == maxFloor) {
+          direction = Direction.DOWN;
+        } else if (currentFloor == minFloor) {
+          direction = Direction.UP;
+        }
+      }
+}
+
+
+
+
+
+public boolean hasUsersToPickup(List<User> users) {
+  for (User user : users) {
+    if (user.getDirection() == direction) {
+      return true;
     }
+  }
+  return false;
+}
+    
 
     public int getId() {
         return id;
@@ -102,4 +147,5 @@ public class Elevator {
     }
 
 }
+
 
