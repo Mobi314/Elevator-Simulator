14e5489430833c98b334d26f67da286352b754b5
Ckurtz79 <68250381+Ckurtz79@users.noreply.github.com>
2023-04-25 21:15:39 -0400

    Update ElevatorSimulator.java

--- ElevatorSimulator.java
+++ ElevatorSimulator.java
@@ -1,45 +1,57 @@
 import java.util.*;
 
 public class ElevatorSimulator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the number of floors: ");
-        int floors = scanner.nextInt();
+    System.out.print("Enter the number of floors: ");
+    int floors = scanner.nextInt();
 
-        System.out.print("Enter the number of elevators: ");
-        int elevators = scanner.nextInt();
+    System.out.print("Enter the number of elevators: ");
+    int elevators = scanner.nextInt();
 
-        System.out.print("Enter the number of users: ");
-        int users = scanner.nextInt();
+    System.out.print("Enter the number of users: ");
+    int users = scanner.nextInt();
 
-        Building building = new Building(floors, elevators);
-        List<User> userList = generateUsers(users, floors);
+    Building building = new Building(floors, elevators);
+    List<User> userList = generateUsers(users, floors);
 
-        while (!userList.isEmpty()) {
-            for (Elevator elevator : building.getElevatorList()) {
-                if (elevator.getDirection() == Direction.NONE && !userList.isEmpty()) {
-                    elevator.setDirection(closestUserDirection(elevator, userList));
-                }
+    boolean allUsersServiced = false;
+    while (!allUsersServiced) {
+        boolean hasUsersToPickup = false;
 
-                elevator.move(userList);
-                displayFrame(elevator, userList);
+        for (Elevator elevator : building.getElevatorList()) {
+            if (elevator.getDirection() == Direction.NONE && !userList.isEmpty()) {
+                elevator.setDirection(closestUserDirection(elevator, userList));
             }
+
+            elevator.move(userList);
+            displayFrame(elevator, userList);
+
+            hasUsersToPickup |= elevator.hasUsersToPickup(userList);
         }
 
-        System.out.println("All users have been serviced.");
+      allUsersServiced = userList.isEmpty() && !hasUsersToPickup && building.getElevatorList().stream()
+                .allMatch(elevator -> elevator.getPassengers().isEmpty());
+
     }
+
+    System.out.println("All users have been serviced.");
+}
 
     public static List<User> generateUsers(int users, int floors) {
         List<User> userList = new ArrayList<>();
         Random random = new Random();
         for (int i = 0; i < users; i++) {
             int startFloor = random.nextInt(floors) + 1;
-            int destinationFloor;
-            do {
-                destinationFloor = random.nextInt(floors) + 1;
-            } while (destinationFloor == startFloor);
-            userList.add(new User(startFloor, destinationFloor));
+            int destinationFloor = random.nextInt(floors) + 1;
+            while (startFloor == destinationFloor) { // ensure start and destination floors are different
+              destinationFloor = random.nextInt(floors) + 1;
+            }
+        int weight = random.nextInt(251) + 50;
+        userList.add(new User(startFloor, destinationFloor, weight));
+            System.out.println("Destination floor for this user: " + destinationFloor
+                  + " Starting floor for this user: " + startFloor);
         }
         return userList;
     }
@@ -62,10 +74,11 @@ public class ElevatorSimulator {
     public static void displayFrame(Elevator elevator, List<User> userList) {
         System.out.println("Elevator ID: " + elevator.getId());
         System.out.println("Direction: " + elevator.getDirection());
-        System.out.println("DisplayState: " + elevator.getDisplayState());
+        //System.out.println("DisplayState: " + elevator.getDisplayState());
         System.out.println("CurrentFloor: " + elevator.getCurrentFloor());
         System.out.println("Users in elevator: " + elevator.getPassengerCount());
         System.out.println("Users waiting: " + userList.size());
+        System.out.println("Total weight: " + elevator.getCurrentWeight());
         System.out.println("--------------------");
     }
 }
