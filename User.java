b741e3d59c463d36536256a8a19499da8fe14516
Ckurtz79 <68250381+Ckurtz79@users.noreply.github.com>
2023-04-25 21:16:04 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,10 +1,21 @@
 public class User {
     private int startFloor;
     private int destinationFloor;
+    private int weight;
 
-    public User(int startFloor, int destinationFloor) {
+    public User(int startFloor, int destinationFloor, int weight) {
         this.startFloor = startFloor;
         this.destinationFloor = destinationFloor;
+        this.weight = weight;
+        
+    if (startFloor > Building.getFloors() || startFloor < 1) {
+        throw new IllegalArgumentException("Invalid start floor");
+    }
+    if (destinationFloor > Building.getFloors() || destinationFloor < 1) {
+        throw new IllegalArgumentException("Invalid destination floor");
+    }
+    this.startFloor = startFloor;
+    this.destinationFloor = destinationFloor;
     }
 
     public int getStartFloor() {
@@ -16,6 +27,11 @@ public class User {
     }
 
     public Direction getDirection() {
-        return startFloor < destinationFloor ? Direction.UP : Direction.DOWN;
+      return startFloor < destinationFloor ? Direction.UP : Direction.DOWN;
+    }
+
+
+    public int getWeight() {
+        return weight;
     }
 }
