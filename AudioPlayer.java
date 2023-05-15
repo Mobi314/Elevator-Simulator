e409d633de8c0db7bd167a6fa2188b2d43706f17
Nihal  Hossain <nhossain@stevens.edu>
2023-05-14 20:22:05 -0400

    maven+elevmusic

---
+++ AudioPlayer.java
@@ -0,0 +1,30 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import javazoom.jl.player.Player;
+
+public class AudioPlayer {
+    private String filename;
+    private Player player; 
+    public AudioPlayer(String filename) {
+        this.filename = filename;
+    }
+    public void close() { if (player != null) player.close(); }
+    public void play() {
+        try {
+            FileInputStream fis     = new FileInputStream(filename);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            player = new Player(bis);
+        }
+        catch (Exception e) {
+            System.out.println("Problem playing file " + filename);
+            System.out.println(e);
+        }
+        new Thread() {
+            public void run() {
+                try { player.play(); }
+                catch (Exception e) { System.out.println(e); }
+            }
+        }.start();
+
+    }
+}
